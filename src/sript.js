62e177d8e17d679a657ac56e3471560662a6b292
TimurIskandarov <tim-darov@rambler.ru>
2020-04-05 14:36:51 +0400

    refactor: change reduce method

--- src/sript.js
+++ src/sript.js
@@ -50,7 +50,7 @@ const keyboard = libraryKeys('EN');
 const rowKeys = [];
 keyboard.forEach((row) => {
   row.generateRowKeys().reduce((a, b) => section.appendChild(b), 0);
-  rowKeys.push(row.reduce((a, b) => (b.length === 1 ? `${a} ${b.className}` : repeat(b)), ''));
+  rowKeys.push(row.reduce((a, b) => (b.length === 1 ? `${a} ${b.className}` : `${a} ${repeat(b)}`), ''));
 });
 console.log(rowKeys);
 section.style.gridTemplateAreas = rowKeys;
